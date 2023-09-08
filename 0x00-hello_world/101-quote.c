6ff487130cb777e087721a4d5b4f7b8b4216a885
Taima <t.kabli02@gmail.com>
2023-09-07 18:51:15 -0700

    Std

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,10 @@
 /**
  * main - general
  * printf -print statement
- * return- always returns 1 (success)
+ * return- always returns 1 (failure)
  */
 int main(void)
 { 
-
-	printf(" and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
