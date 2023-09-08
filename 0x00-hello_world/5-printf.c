b6b65a8326785ff362eaaf96ca9bbc46f2852898
Taima <t.kabli02@gmail.com>
2023-09-07 18:14:49 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stio.h>
+/**
+ * main - Entry point
+ * Description: a c program that uses put finction
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return(0);
