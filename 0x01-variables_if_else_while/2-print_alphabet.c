ad4a0e7b06d564b94ce581f6e5dfd7ec57767e25
Taima <t.kabli02@gmail.com>
2023-09-08 17:48:32 -0700

    Printing the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - main function
+ * A function that prints the alphabet
+ * in lower case
+ * Return: always returns 0 (success)
+ */
+int main(void)
+{
+	char ch="abcdefghijklmnopqrstuvwxyz\n";
+	putchar(ch);
+	return (0);
+}
+
