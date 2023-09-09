b45c75c6170c8758f8a5f0e27cefc00ada49b3cc
Taima <t.kabli02@gmail.com>
2023-09-08 17:21:57 -0700

    Nested if with two variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
  * main - main function
  * description: a function that prints last digit of an integer
  * and decides whether it is less than 5, zero, or else.
- * Return - return value always 0 (success)
+ * Return:  return value always 0 (success)
  */
 int main(void)
 {
@@ -18,9 +18,8 @@ int main(void)
 	if (digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 	else if (digit == 0)
-	{
 		printf("Last digit of %d is %d and is 0\n", n, digit);
-	} else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);	
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	return (0);
 }
