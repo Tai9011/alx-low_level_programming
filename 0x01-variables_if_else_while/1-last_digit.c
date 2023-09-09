f02d999309ae2ce5a56c539fd2ca79f5f495713c
Taima <t.kabli02@gmail.com>
2023-09-08 17:13:02 -0700

    Nested if with two variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 /**
  * main - main function
- * description: a function that prints last digit of an integer and decides whether it is less than 5, zero, or else.
+ * description: a function that prints last digit of an integer
+ * and decides whether it is less than 5, zero, or else.
  * Return - return value always 0 (success)
  */
 int main(void)
@@ -14,12 +15,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	digit = n % 10;
-	if(digit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n",n,digit);
-	else if(digit==0)
-		printf("Last digit of %d is %d and is 0\n",n,digit);
+	if (digit > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
+	else if (digit==0)
+		printf("Last digit of %d is %d and is 0\n", n, digit);
 	else
-	printf("Last digit of %d is %d and is less than 6 and not 0\n",n,digit);	
-	
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);	
 	return (0);
 }
