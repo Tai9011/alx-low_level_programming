cb9929e4830e201c1b7840786267cbfcd69821bc
Taima <t.kabli02@gmail.com>
2023-09-08 19:56:53 -0700

    Last call

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - test point
+ * description: function that prints combinations
+ * of single digit numbers
+ * Return: always returns a value of 0 (success)
+ */
+int main(void)
+{
+	int dg = 0;
+
+	while (dg < 10)
+	{
+		putchar(dg);
+		putchar(", ");
+		dg++;
+	return (0);
+}
