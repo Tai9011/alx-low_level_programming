de03b6903dc1bae0a53b1f4643a4b7f0971541eb
Taima <t.kabli02@gmail.com>
2023-09-08 17:18:26 -0700

    Nested if with two variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	else if (digit == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, digit);
-	}else
+	} else
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);	
 	return (0);
 }
