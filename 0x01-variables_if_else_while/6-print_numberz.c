813f92c62f9795407ddf946faf5c8aa5f0f9e414
Taima <t.kabli02@gmail.com>
2023-09-08 19:34:53 -0700

    Printing numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * description: a function that prints single digit numbers
+ * using putchar
+ * Return: always returns a value of 0 (success)
+ */
+int main(void)
+{
+	int di = 0;
+
+	while(di < 10)
+	{
+		putchar(di);
+		di++;
+	}
+	putchar('\n');
+	return (0);
+}
+
