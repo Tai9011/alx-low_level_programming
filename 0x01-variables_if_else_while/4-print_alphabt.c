8999beed798fa4b500c063ef355fbcc98e1e0f53
Taima <t.kabli02@gmail.com>
2023-09-08 18:46:51 -0700

    Letter exceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - main function
+ * A function that prints the alphabet
+ * in lower case
+ * Return: always returns 0 (success)
+ */
+int main(void)
+{
+        char ch = 'a';
+
+        while (ch != 'q'&& ch != 'e')
+                putchar(ch);
+		ch++;
+        putchar('\n');
+        return (0);
+
+}
