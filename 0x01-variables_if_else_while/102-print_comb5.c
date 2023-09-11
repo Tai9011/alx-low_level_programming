89c9d735e866c4e1648321cbaddccecacb9ca793
Taima <t.kabli02@gmail.com>
2023-09-11 05:40:51 -0700

    Printing nbs to 99

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * description: print nbs. 1 to 99
+ * Return: always returns a value of 0 (success)
+ */
+int main(void)
+{
+	dg1 = 0;
+	dg2;
+
+	while (dg1 <= 9)
+	{
+		dg2 = 0;
+		while (dg2 <= 9)
+		{
+			dg2++;
+		}
+		dg1++;
+	}
+	return (0);
+}
