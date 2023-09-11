d98f1bfedbf3f5b82d1e1ba21b7eafcfc4ac2fba
Taima <t.kabli02@gmail.com>
2023-09-11 02:38:03 -0700

    Printing combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,15 @@ int main(void)
 	int s = 0;
 	int r = 0;
 	
-	while ( s <= 9 && r <= 9 && s < r)
+	while ( s <= 9 && r <= 9)
 	{
+		if (s < r)
+		{
 		putchar(s + 0);
 		putchar(r + 0);
 		putchar(44);
 		putchar(32);
+		}
 		s++;
 		r++;
 	}
