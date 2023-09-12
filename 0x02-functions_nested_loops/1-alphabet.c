dd043ad04c892e1bcd8661ffdf0005ce7c61163f
Taima <t.kabli02@gmail.com>
2023-09-12 05:49:29 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,14 @@
 #include "main.h"
+#include<stdio.h>
 
 /**
  * main - check the code
- *
+ * description: dunction prints the alphabet
  * Return: Always 0.
  */
+void print_ alphabet(void);
 int main(void)
 {
-    print_alphabet();
-    return (0);
+	print_alphabet();
+	return (0);
 }
