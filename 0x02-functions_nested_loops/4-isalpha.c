51296e806058ec9ba90381e00bfb733a232c65c3
Taima <t.kabli02@gmail.com>
2023-09-12 16:58:02 -0700

    TRial

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,10 @@
  */
 int _isalpha(int c)
 {
-	if ('A' <= c <= 'Z')
-		c++;
-	return (0);
+	if (c <= 'Z' && c >= 'A')
+		return (1);
+	else if (c <= 'z' && c >= 'a')
+		return (1);
+	else
+		return (0);
 }
