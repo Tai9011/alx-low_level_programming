3404cc0cac483785f1bf39a1c30787c08e8af252
Taima <t.kabli02@gmail.com>
2023-09-12 16:43:42 -0700

    Printing lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
-/*
- * int _islower(int c) - a function that detects lowercase
- * _islower: checks whether a letter is lowercase
+/**
+ * _islower - checks lowercase
+ * @c: description
  * Return: returns 1 when lower and returns 0 when not
  */
 int _islower(int c)
