e76d30df651163dc8903e8a129a83c482ba983db
Taima <t.kabli02@gmail.com>
2023-09-12 02:34:14 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<unistd.h>
+#include<main.h>
+/**
+ * print - prints _putchar
+ * description: writing my own function
+ * Return: always returns 0 (success)
+ * char: a character
+ */
+int print(char)
+{
+	_putchar('_putchar');
+	_putchar('\n');
+	return (0);
+}
