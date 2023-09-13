223a7ba34345fa314d5c641f9d60333a0cad5d14
Taima <t.kabli02@gmail.com>
2023-09-13 03:17:42 -0700

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_to_98 - print
+ * @n: variable
+ */
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		_putchar(n + 48);
+		if (n != 98)
+		{
+		_putchar(',');
+		_putchar(' ');
+		}
+		n++;
+	}
+
+}
