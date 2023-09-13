421d40613e9a56c760c054926295dd16139e083c
Taima <t.kabli02@gmail.com>
2023-09-13 00:57:15 -0700

    Printing time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,39 +20,3 @@ void jack_bauer(void)
 		}
 	}
 }
-/*
- * int srt[]="00:56"
- * if (minute < 10)
- * {
- * 	if (hour < 10)
- * 	{
- * 	 _putchar(0);
- * 	_putchar(hour);
- * 	_putchar(':');
- * 	_putchar(0);
- * 	_putchar(minute);
- * 	}
- *      _putchar(hour);
- *      _putchar(':');
- *      _putchar(0);
- *      _putchar(minute);
- * }
- *
- *if (hour < 10)
- {
-	*      if (minute < 10)
- *      {
- *       _putchar(0);
- *      _putchar(hour);
- *      _putchar(':');
- *      _putchar(0);
- *      _putchar(minute);
- *      }
- *      _putchar(0);
- *      _putchar(hour);
- *      _putchar(':');
- *      _putchar(minute);
- * }
- *
- }
- */
