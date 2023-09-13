ac23e19741eacc1affef9f781b54ccfebbc22855
Taima <t.kabli02@gmail.com>
2023-09-13 02:54:52 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * description: adding
+ * @a: integer
+ * @b: integer
+ * Return: returns sum
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
