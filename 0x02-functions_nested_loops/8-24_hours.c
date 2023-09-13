497d3d699b404c9af047b63e93977efed55bf26a
Taima <t.kabli02@gmail.com>
2023-09-13 01:08:21 -0700

    Printing time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /**
- *
- *
- *
+ * jack_bauer - prints time
  */
 void jack_bauer(void)
 {
 	int hour = 0;
 	int minute;
 
-	while (hour <= '23')
+	while (hour <= 23)
 	{
 		minute = 0;
-		while (minute <= '59')
+		while (minute <= 59)
 		{
 			_putchar(hour);
 			_putchar(':');
