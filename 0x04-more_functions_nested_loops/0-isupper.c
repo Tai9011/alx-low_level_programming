1a684b59398088bb6aea06e4f6c052f290ec8062
Taima <t.kabli02@gmail.com>
2023-09-14 16:47:11 -0700

    uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * _isupper - detects uppercase
- *
- *
+ * @c: integer
+ * Return: returns 0 or 1
  */
 int _isupper(int c)
 {
-	int c = 'A';
-	if (c >= 'A' && c <= 'Z')
+	int c = 0;
+
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
