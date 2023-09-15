a4180a0b57b0858ddec7fef6f418e1e614aaa928
Taima <t.kabli02@gmail.com>
2023-09-15 16:58:32 -0700

    Add

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,19 @@ void more_numbers(void)
 {
 	int nb;
 	int i = 0;
+	int dg;
 
 	while (i <= 9)
 	{
 		while (nb <= 14)
 		{
+			dg = nb;
 			if (nb > 9)
 			{
 				_putchar(1 + 48);
-				_putchar((nb % 10) + 48);
+				dg = nb % 10;
 			}
+			_putchar(dg + 48);
 			nb++;
 
 		}
