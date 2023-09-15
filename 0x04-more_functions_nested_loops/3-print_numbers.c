de66b4ff16d41bddb191625d53eb06d84f4d75a5
Taima <t.kabli02@gmail.com>
2023-09-15 16:14:05 -0700

    Print digits

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * print_numbers: prints numbers
+ * print_numbers - prints numbers
  * @i: variable
  */
 void print_numbers(void)
 {
 	int i = 48;
+
 	for (i = 48; i <= 57; i++)
 		_putchar(i);
 	_putchar('\n');
