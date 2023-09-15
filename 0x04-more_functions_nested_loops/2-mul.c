959ca9b5cedede5f9354cc0372e38f5aac7382bd
Taima <t.kabli02@gmail.com>
2023-09-15 04:13:19 -0700

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two digits
+ * @a: variable
+ * @b:variable
+ * Return: returns mult
+ */
+int mul(int a, int b)
+{
+	int mult = a * b;
+	return (mult);
+}
