2f47ab389aceaf257a939a9c0d90e1a17aa6c172
Taima <t.kabli02@gmail.com>
2023-09-21 17:08:07 -0700

    Pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcat - idk
+ * @dest: variable
+ * @src: variable
+ * Return: returns dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int c, c2;
+
+	c = 0;
+	while (dest[c])
+		c++;
+	for (c2 = 0; src[c2]; c2++)
+		dest[c++] = scr[c2];
+	return (dest);
+}
