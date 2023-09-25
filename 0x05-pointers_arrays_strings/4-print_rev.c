f176ed245e0fdc515fc375139bb1e0e8c925283c
Taima <t.kabli02@gmail.com>
2023-09-25 02:39:07 -0700

    String reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,7 +5,11 @@
  */
 void print_rev(char *s)
 {
-	 for (*s = '\0'; *s >= 0; s--)
-		 _putchar(*s + 0);
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar (s[i]);
 	 _putchar ('\n');
 }
